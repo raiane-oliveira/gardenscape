b2dd7ea646adc48ba9209404ac62a2dee2c9e0fe
Raiane <raiane.oliveira404@gmail.com>
2024-04-13 13:53:22 -0300

    chore: add packages and modify env example file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "d2seqvvyy3b8p2.cloudfront.net",
+      },
+      {
+        protocol: "https",
+        hostname: "bs.plantnet.org",
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
