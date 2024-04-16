bed42647f936073f53e0128e2f2c3dc1033899a1
Raiane <raiane.oliveira404@gmail.com>
2024-04-16 11:46:45 -0300

    refactor: add missing config object to function

--- src/utils/api-trefle.ts
+++ src/utils/api-trefle.ts
@@ -1,13 +1,13 @@
 // TODO: fix env schema
 // import { env } from "@/env"
 
-import axios from "axios"
+import axios, { AxiosRequestConfig } from "axios"
 
-export function apiTrefle(path: string) {
+export function apiTrefle(path: string, config?: AxiosRequestConfig) {
   const baseUrl = process.env.TREFLE_BASE_API_URL
   const url = new URL(`/api/v1${path}`, baseUrl)
 
   url.searchParams.append("token", process.env.TREFLE_API_TOKEN ?? "")
 
-  return axios.get(url.toString())
+  return axios.get(url.toString(), config)
 }
